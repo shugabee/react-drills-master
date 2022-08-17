f89cb82fafa689cfd83fc1babef5290b132df0e5
Shelby Webb <shelbylyn.webb@gmail.com>
2022-08-16 21:08:19 -0600

    app3

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      filerString: "",
+      foods: ["spaghetti", "ice cream", "sushi", "bologna", "cheese"]
+    };
+  }
+
+  handleChange(filter) {
+    this.setState({ filterString: filter })
+  }
+
+  render() {
+    let foodsToDisplay = this.state.foods
+      .filter((element, index) => {
+        return element.includes(this.state.filerString);
+      })
+      .map((element, index) => {
+        return <h2 key={index}>{element}</h2>;
+      });
+
+    return (
+      <div className='App'>
+        <input onChange={e => this.handleChange(e.target.value)} type="text" />
+        {foodsToDisplay}
+      </div>
+    );
+}
+}
+
+export default App;
