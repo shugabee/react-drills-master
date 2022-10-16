5e1e2bd495ba847dd3c4d72713e10272a7eebcad
Shelby Webb <shelbylyn.webb@gmail.com>
2022-10-15 18:34:13 -0600

    app8

--- Desktop/WebDev/react-drills-master/app-8/src/App.js
+++ Desktop/WebDev/react-drills-master/app-8/src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+import axios from "axios";
 
 function App() {
+  const [lyrics, setLyrics] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://taylorswiftapi.herokuapp.com/get").then((res) => {
+      console.log(res.data.quote);
+      setLyrics(res.data.quote);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Taylor Swift is LYFE</h2>
+      {/* <button onClick={clickHandler}>Give me a lyric</button> */}
+
+      <div>{lyrics}</div>
     </div>
   );
 }
